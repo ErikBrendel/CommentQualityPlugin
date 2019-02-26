4ee8ccdc517bc34fce6e863726a4620e0fd7120e
Anton von Weltzien <a.weltzien@outlook.de>
2019-02-26 14:42:40 +0100

    Add group by for stuff

--- python/test.py
+++ python/test.py
@@ -2,13 +2,17 @@ import pandas as pd
 import glob
 import os
 import sys
+from textblob import TextBlob
+from textblob.classifiers import DecisionTreeClassifier
 
 REPO_ROOT =  "commentMetrics"
 
 metrics_files = os.path.join(REPO_ROOT, "**", "*.csv")
 
 for filename in glob.iglob(metrics_files, recursive=True):
-    data = pd.read_csv(filename, sep=';').rename(columns={"# id": "id"}).set_index(
-        ['id', 'timestamp'])
-    data.to_csv(sys.stdout)
+    data = pd.read_csv(filename, sep=';').rename(columns={"# id": "id"})
+    data['survivalAmount'] = 1
+    data2 = data.groupby(["id", "commentWords", "codeWords"]).agg({"survivalAmount": pd.np.sum})
+    data2.to_csv(sys.stdout)
     print(filename)
+    break
