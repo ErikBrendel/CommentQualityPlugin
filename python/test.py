c0663165069f66604249a1a91cf0343380dcc7a3
Erik Brendel <erik.brendel@gmx.de>
2019-02-26 14:02:15 +0100

    first bits of python code

--- python/test.py
+++ python/test.py
@@ -1,3 +1,14 @@
-import pandas
+import pandas as pd
+import glob
+import os
+import sys
 
-print(pandas)
+REPO_ROOT = "/home/erik/Documents/repos/qualityCommentRepos/"
+
+for file in os.listdir(REPO_ROOT):
+    metrics_files = os.path.join(REPO_ROOT, file, "commentMetrics", "**", "*.csv")
+
+    for filename in glob.iglob(metrics_files, recursive=True):
+        data = pd.read_csv(filename, sep=';').rename(columns={"# id": "id"}).set_index(['id', 'timestamp'])
+        data.to_csv(sys.stdout)
+        print(filename)
