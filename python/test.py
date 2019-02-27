da027c14ce67c08620ea7d0cec3b0689c183ae76
Anton von Weltzien <a.weltzien@outlook.de>
2019-02-27 10:10:29 +0100

    Add caching for dataFrame

--- python/test.py
+++ python/test.py
@@ -6,12 +6,15 @@ from textblob import TextBlob
 from textblob.classifiers import DecisionTreeClassifier
 
 REPO_ROOT = "commentMetrics"
-
+SHOULD_CACHE = True
 metrics_files = os.path.join(REPO_ROOT, "**", "*.csv")
-
-result = []
+comment_frame = None
+comment_list = []
 
 for filename in glob.iglob(metrics_files, recursive=True):
+    if os.path.isfile('cache') and SHOULD_CACHE:
+        comment_frame = pd.read_pickle('cache')
+        break
     df = pd.read_csv(filename, sep=';').rename(columns={"# id": "id"})
     df.fillna('', inplace=True)
     # data.to_csv(sys.stdout, sep=";")
@@ -27,7 +30,7 @@ for filename in glob.iglob(metrics_files, recursive=True):
         for key, row_data in versions_data.iloc[1:].iterrows():
             if row_data['commentWords'] != old_comment and row_data['codeWords'] == old_code \
                     and abs(len(row_data['commentWords']) - len(old_comment)) > 3:
-                result.append({
+                comment_list.append({
                     'id': comment_id,
                     'timestamp': old_timestamp,
                     'commentWords': old_comment,
@@ -35,9 +38,9 @@ for filename in glob.iglob(metrics_files, recursive=True):
                     'commentText': old_comment_text,
                     'codeText': old_code_text,
                     'label': 'bad'})
-                if len(result) >= 2 and result[-2]['id'] == result[-1]['id']:
-                    result.pop(-2)
-                result.append({
+                if len(comment_list) >= 2 and comment_list[-2]['id'] == comment_list[-1]['id']:
+                    comment_list.pop(-2)
+                comment_list.append({
                     'id': comment_id,
                     'timestamp': row_data['timestamp'],
                     'commentWords': row_data['commentWords'],
@@ -57,5 +60,9 @@ print()
 print()
 
 cols = ['id', 'timestamp', 'label', 'commentWords', 'codeWords', 'commentText', 'codeText']
-result = pd.DataFrame(result, columns=cols)
-result.to_csv(sys.stdout, sep=';')
+comment_frame = comment_frame if comment_frame is not None else pd.DataFrame(comment_list,
+                                                                          columns=cols)
+comment_frame.to_pickle('cache')
+data = comment_frame[['commentWords', 'codeWords', 'label']]
+
+data.to_csv(sys.stdout, sep=';')
