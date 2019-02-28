c0c0f62ad211c63a353143e35ab28dfe7889a441
Anton von Weltzien <a.weltzien@outlook.de>
2019-02-28 11:20:00 +0100

    Add scikit learn pipeline with basic tfidf vector and svm. Just for comment words right now

--- python/test.py
+++ python/test.py
@@ -1,13 +1,10 @@
-from typing import *
-
 import pandas as pd
 import numpy as np
-from pandas import DataFrame
+from pandas import DataFrame, Series
+from sklearn.linear_model import SGDClassifier
 
 from sklearn.model_selection import train_test_split
 from utilities import to_textblob_format, read_and_cache_data, export_csv
-import sys
-import math
 import os
 
 REPO_ROOT = os.getenv('CSV_ROOT', "commentMetrics")
@@ -26,10 +23,25 @@ x_train, x_test, y_train, y_test = train_test_split(comment_and_code_words, labe
                                                     random_state=42)
 print('good:', len(labels[labels['label'] == 'good']), 'bad:',
       len(labels[labels['label'] == 'bad']))
-train = to_textblob_format(x_train, y_train)
-test = to_textblob_format(x_test, y_test)
-
-
+#train = to_textblob_format(x_train, y_train)
+#test = to_textblob_format(x_test, y_test)
 
 
 # print(train)
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+text_clf = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                          alpha=1e-3, random_state=42,
+                          max_iter=5, tol=None)),
+])
+text_clf.fit(x_train['commentWords'], y_train.values.ravel())
+predicted = text_clf.predict(x_test['commentWords'])
+#print(predicted)
+from sklearn import metrics
+print(metrics.classification_report(y_test, predicted))
+print(metrics.confusion_matrix(y_test, predicted))
